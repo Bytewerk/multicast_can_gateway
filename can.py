bc6c340731e788538cf5dc38d7316fc5ba7a16c2
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-09-18 22:59:58 +0200

    add __str__(), __repr__(), size, unpack() to CANMessage

--- can.py
+++ can.py
@@ -2,6 +2,7 @@ import struct
 
 class CANMessage():
 	msg_fmt = "=IB3x8s"
+	size = struct.calcsize(msg_fmt)
 
 	def __init__(self, canid=0x0, data=b''):
 		self.canid = canid
@@ -17,3 +18,13 @@ class CANMessage():
 
 	def __bytes__(self):
 		return self.pack()
+
+	def __str__(self):
+		return repr(self)
+
+	def __repr__(self):
+		return "CANMessage(canid={0:x},dlc={1},data={2!r})".format(self.canid, self.dlc, self.data)
+
+def unpack(msg_bytes):
+	canid, dlc, data = struct.unpack(CANMessage.msg_fmt, msg_bytes)
+	msg = CANMessage(canid, data)
